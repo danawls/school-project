b7bfe506610ceca4077d4959b598fc3d6d2416c7
woojin <114149553+danawls@users.noreply.github.com>
2024-10-21 00:41:04 +0900

    aa

--- app.js
+++ app.js
@@ -50,4 +50,4 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(3000);
+module.exports = app;
--- public/scripts/present.js
+++ public/scripts/present.js
@@ -4,11 +4,13 @@ let isDid = false;
 
 const ne = document.querySelector("#next");
 
-let presents = localStorage.getItem("presents")? localStorage.getItem("presents").split(","): ["선물을 입력하세요."];
+let presents = localStorage.getItem("presents") ? localStorage.getItem("presents").split(","): ["선물을 입력하세요."];
+
 
 if (presents[0] !== "선물을 입력하세요.") {
-    if(presents.length < 1) {
+    if(presents.length < 2) {
         presents = [presents[0].split('"')[1]];
+        console.log(presents);
     }
 }
 
@@ -16,14 +18,17 @@ const presentId = Math.floor(Math.random() * presents.length);
 
 function drawGift(){
     if(!isDid) {
+        console.log(presentId);
         giftPaper.textContent = presents[presentId]; // Replace with actual gift name logic
         const vv = localStorage.getItem("num");
         if(vv) {
             const ss = JSON.parse(vv);
+            console.log(ss);
             ss[presents[presentId]] = Number(ss[presents[presentId]]) - 1;
             console.log(ss[presents[presentId]]);
             if(Number(ss[presents[presentId]]) < 1) {
                 delete ss[presents[presentId]];
+                console.log(presents);
                 presents.splice(presents.indexOf(presents[presentId]), 1);
                 localStorage.setItem('num', JSON.stringify(ss));
                 if (presents.length < 1) {
